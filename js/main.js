508c0cc2d036d060de4faae335c144c7025a8899
Daniel <>
2018-07-08 13:26:30 -0400

    Added external JSON level data

--- js/main.js
+++ js/main.js
@@ -7,11 +7,20 @@ var GameState = {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
+
+    //Set physics engine
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.game.physics.arcade.gravity.y = 1000;
+
+    //Set world bounds
+    this.game.world.setBounds(0,0,360,700); 
+
+    //Set game constants
     this.RUNNING_SPEED = 180;
     this.JUMPING_SPEED = 550;
   },
+
+
 
   //load the game assets before the game starts
   preload: function() {
@@ -24,11 +33,16 @@ var GameState = {
 
     this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1);
     this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);
+
+    this.load.text('level', 'assets/data/level.json');
   },
   //executed after everything is loaded
   create: function() {
+    // parse level data
+    this.levelData = JSON.parse(this.game.cache.getText('level'));
+
     //create ground
-    this.ground = this.add.sprite(0, 500, 'ground');
+    this.ground = this.add.sprite(0, 630, 'ground');
     this.game.physics.arcade.enable(this.ground);
     this.ground.body.allowGravity = false;
     this.ground.body.immovable = true;
@@ -42,18 +56,21 @@ var GameState = {
     ]
     this.platforms = this.add.group();
     this.platforms.enableBody = true;
-    platformData.forEach((data) => {
+    this.levelData.PlatformData.forEach((data) => {
       this.platforms.create(data.x, data.y, 'platform');
     }, this)
     this.platforms.setAll('body.allowGravity', false);
     this.platforms.setAll('body.immovable', true);
 
     //create player
-    this.player = this.add.sprite(100, 200, 'player', 3);
+    this.player = this.add.sprite(this.levelData.PlayerStart.x, this.levelData.PlayerStart.y, 'player', 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
     this.game.physics.arcade.enable(this.player);
     this.player.body.velocity.x = 0;
+
+    //Set camera controls
+    this.game.camera.follow(this.player);
 
     //create keyboard controls
     this.cursors = this.game.input.keyboard.createCursorKeys();
